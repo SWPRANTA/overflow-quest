22d41d3c0339ce24771ebfbbf920b0a6b7c04c5b
SwapnilPranta <200204@ku.ac.bd>
2024-10-21 02:11:33 +0600

    First Commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Hello from OverflowQuest!")
+st.write("Search your stackoverflow questions here!")
+
+
+@st.cache_data
+def load_data():
+    train_df = pd.read_csv('train.csv')
+    term_doc_matrix = pd.read_csv('term_doc_matrix.csv', index_col=0)
+    return train_df, term_doc_matrix
+
+
+train_df, term_doc_matrix = load_data()
+
+# User input query
+query = st.text_input("Enter your query:")
+
+if query:
+    # Split the query into words and lower them
+    query_words = query.lower().split()
+
+    # Find the documents (columns) in term-doc-matrix that contain the query words
+    matched_docs = pd.DataFrame()
+
+    for word in query_words:
+        if word in term_doc_matrix.index:
+            matched_docs = pd.concat(
+                [matched_docs, term_doc_matrix.loc[word]], axis=1)
+
+    if not matched_docs.empty:
+        # Sum word counts across all query words and sort documents by their count
+        doc_scores = matched_docs.sum(axis=1)
+        doc_scores = doc_scores[doc_scores > 0].sort_values(ascending=False)
+        doc_ids = doc_scores.index.astype(int)
+
+        # Display the top documents from train.csv
+        st.write(f"Documents matching '{query}':")
+
+        for doc_id in doc_ids[:11]:
+            doc = train_df.iloc[doc_id]
+            # st.write(f"**Score:** {doc_scores[str(doc_id)]}")
+            # Color the score text (you can choose any color)
+            colored_score_text = f'<span style="color: green; font-weight: bold;"><b>Score:</b> {doc_scores[str(doc_id)]}</span>'
+
+            # Displaying the colored score
+            st.markdown(colored_score_text, unsafe_allow_html=True)
+            st.write(f"**ID:** {doc['Id']}")
+            st.write(f"**Created At:** {doc['CreationDate']}")
+            st.write(f"**Title:** {doc['Title']}")
+            st.write(f"**Body:** {doc['Body']}")
+            st.write("---")
+    else:
+        st.write("No matching documents found.")
